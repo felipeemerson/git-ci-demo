aee046e408020c23d1bed90d700c22010c31a233
Felipe <felipeemersonsb@hotmail.com>
2022-01-31 17:20:45 -0300

    Creating the demo project with 3 passing tests.

---
+++ src/main/java/com/example/gitcidemo/GitCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitcidemo/GitCiDemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.gitcidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class GitCiDemoApplicationTests {
+
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+    
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+
+}
